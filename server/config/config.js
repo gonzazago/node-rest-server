12df49b413e5acc411397fe47d183c50cb392206
Arroyo <HArroyo@meridionalseguros.com.ar>
2019-03-22 10:24:44 -0300

    Variable de Entorno

--- server/config/config.js
+++ server/config/config.js
@@ -12,13 +12,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 /**
  * CONFIGURACION VD
  */
-let urlMongo = "mongodb+srv://admin-gsz:J4NnXHWqQkPFmIzU@cluster0-iol1z.mongodb.net/cafe?retryWrites=true"
+//let urlMongo = "mongodb+srv://admin-gsz:J4NnXHWqQkPFmIzU@cluster0-iol1z.mongodb.net/cafe?retryWrites=true"
+
+let urlMongo = process.env.MONGO_URI
 let urlLocal = "mongodb://localhost:27017/cafe"
 let urlConnectionDb;
 
 if (process.env.NODE_ENV === 'dev') {
     urlConnectionDb = urlLocal;
 } else {
+    console.log(urlMongo);
     urlConnectionDb = urlMongo;
 }
 
