e068e577236febd3ab3ff93fd084cb85058cf210
Arroyo <HArroyo@meridionalseguros.com.ar>
2019-03-22 10:27:33 -0300

    Remplazo url Conexion por Variable de entonro

--- server/config/config.js
+++ server/config/config.js
@@ -12,8 +12,6 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 /**
  * CONFIGURACION VD
  */
-//let urlMongo = "mongodb+srv://admin-gsz:J4NnXHWqQkPFmIzU@cluster0-iol1z.mongodb.net/cafe?retryWrites=true"
-
 let urlMongo = process.env.MONGO_URI
 let urlLocal = "mongodb://localhost:27017/cafe"
 let urlConnectionDb;
